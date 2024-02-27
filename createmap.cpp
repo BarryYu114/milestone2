fbab679f11d0210a12c2a9a9035e309281318761
BarryYu114 <139152335+BarryYu114@users.noreply.github.com>
2024-02-27 14:36:41 -0500

    Update createmap.cpp
    add notes 02/27/2024 ezgl drawmap

--- createmap.cpp
+++ createmap.cpp
@@ -1,1 +1,24 @@
+//screen redraw function
+//put a pointer that need to draw to stak
+//querus easygeo doumentation !!easy to read
+draw_main_canas(ezgl::renderer *g){
+  - g->set_color(ezgl::BLACK);
+  - g->set_color(0,0,0,255); //255 mean opaque draw twice the faster
+  - g->set_line_width (3); //3 pixels wide
+  //stiky, stay until you change them
+  - g->set_line_dash (ezgl::line_dash::asymmetric_5_3);
+}
 
+//Draw primitives
+- g->draw_line (point1, point2); //from ezgl example program
+
+//sample ezgl function
+//the redrawfunction will basically call helper functions
+//redraw functions is being called all the time
+
+//0 is black and 255 is broght blue, last parameter to control how transparent the line is
+//set pixel width probably
+//since called at last, the blue line will on top of all other figures
+g->set_line_width(100)
+g->set_color(0,0,120,125);
+g->draw_line({0,0}, {1000,1050});
